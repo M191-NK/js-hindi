59a3430ee5b4365d42347a850bc0839f87115250
M191-NK <mkgoyal2005@gmail.com>
2026-01-11 10:23:29 +0000

    container config done

---
+++ js_basics/01_var.js
@@ -0,0 +1,18 @@
+const account_id = 90889
+let accountEmail = "mayank@gamil.com"
+var accountPassword = "23345"
+acccountCity = "Delhi"
+let accountState;
+
+//account_id = 2
+
+accountEmail = "hc@hc.com"
+acccountCity = "kolkata"
+accountPassword = "78465"
+
+/*
+prefer not to use var in block_scope 
+or in functional_scope
+*/
+console.log(account_id)
+console.table([account_id,accountEmail,accountPassword,acccountCity,accountState])
